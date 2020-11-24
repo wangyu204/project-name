d62c367865ec22f62810dc04225cf35b00a8f8dc
王喻 <wangyu@ewell.cc>
2020-11-24 16:24:47 +0800

    修改内容：编译成功提交

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { UserService } from './logical/user/user.service';
-// import { UserController } from './logical/user/user.controller';
 import { UserModule } from './logical/user/user.module';
-import { AuthService } from './logical/auth/auth.service';
+// import { AuthService } from './logical/auth/auth.service';
 import { AuthModule } from './logical/auth/auth.module';
 
 @Module({
   imports: [UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, AuthService],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
