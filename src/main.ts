c68962bde3e58eb75dcb17068d7d0de63dec36be
王喻 <wangyu@ewell.cc>
2020-11-25 13:14:26 +0800

    修改内容：中间件实现日志打印

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { logger } from './middleware/logger.middleware';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.json()); // For parsing application/json
+  app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+  // 监听所有的请求路由，并打印日志
+  app.use(logger);
   app.setGlobalPrefix('wzy'); // 全局路由前缀
   await app.listen(3000);
 }
