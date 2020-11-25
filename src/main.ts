a07e8b832eddc023ef09c551608dcf929f5c2ee0
王喻 <wangyu@ewell.cc>
2020-11-25 13:22:40 +0800

    修改内容：返回参数日志打印实现

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './middleware/logger.middleware';
 import * as express from 'express';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
   app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
   // 监听所有的请求路由，并打印日志
   app.use(logger);
+  // 使用全局拦截器打印出参
+  app.useGlobalInterceptors(new TransformInterceptor());
   app.setGlobalPrefix('wzy'); // 全局路由前缀
   await app.listen(3000);
 }
