5e92dadb36515b079cf76cc2910bd3cd1790e79d
王喻 <wangyu@ewell.cc>
2020-11-25 13:50:47 +0800

    修改内容：异常捕获机制添加

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { logger } from './middleware/logger.middleware';
 import * as express from 'express';
 import { TransformInterceptor } from './interceptor/transform.interceptor';
+import { HttpExceptionFilter } from './filter/http-exception.filter';
+import { AllExceptionsFilter } from './filter/any-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,10 @@ async function bootstrap() {
   // 使用全局拦截器打印出参
   app.useGlobalInterceptors(new TransformInterceptor());
   app.setGlobalPrefix('wzy'); // 全局路由前缀
+  //全部其他类型异常
+  app.useGlobalFilters(new AllExceptionsFilter());
+  // 过滤处理 HTTP 异常
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
