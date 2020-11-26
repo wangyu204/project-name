9e7926cb460b7608f9b162ff739bf8b088c88db1
王喻 <wangyu@ewell.cc>
2020-11-26 14:33:46 +0800

    修改内容：权限问题优化

--- src/logical/auth/constants.ts
+++ src/logical/auth/constants.ts
@@ -1,4 +1,11 @@
-// src/logical/auth/constats.ts
+// src/logical/auth/constants.ts
 export const jwtConstants = {
-    secret: 'shinobi7414' // 秘钥
+    secret: 'shinobi7414',
+};
+
+export const roleConstans = {
+    SUPER_ADMIN: 0, // 超级管理员
+    ADMIN: 1, // 管理员
+    DEVELOPER: 2, // 开发者（测试、运营具有同一权限，若提升为 RBAC 1 以上，则可酌情分开）
+    HUMAN: 3 // 普通用户
 };
--- src/logical/commodity/commodity.controller.ts
+++ src/logical/commodity/commodity.controller.ts
@@ -3,40 +3,41 @@ import { Controller, Request, Post, Body, UseGuards, UseInterceptors } from '@ne
 import { AuthGuard } from '@nestjs/passport';
 import { CommodityService } from './commodity.service';
 import { RbacInterceptor } from '../../interceptor/rbac.interceptor';
+import { roleConstans as role } from '../auth/constants'; // 引入角色常量
 
 @Controller('commodity')
 export class CommodityController {
-  constructor(private readonly commodityService: CommodityService) {}
+    constructor(private readonly commodityService: CommodityService) { }
 
-  // 查询商品列表
-  @UseGuards(AuthGuard('jwt'))
-  @UseInterceptors(new RbacInterceptor(3)) // 调用 RBAC 拦截器
-  @Post('list')
-  async queryColumnList(@Body() body: any) {
-    return await this.commodityService.queryCommodityList(body);
-  }
+    // 查询商品列表
+    @UseGuards(AuthGuard('jwt'))
+    @UseInterceptors(new RbacInterceptor(role.HUMAN))
+    @Post('list')
+    async queryColumnList(@Body() body: any) {
+        return await this.commodityService.queryCommodityList(body);
+    }
 
-  // 新建商品
-  @UseGuards(AuthGuard('jwt'))
-  @UseInterceptors(new RbacInterceptor(2))
-  @Post('create')
-  async createCommodity(@Body() body: any, @Request() req: any) {
-    return await this.commodityService.createCommodity(body, req.user.username);
-  }
+    // 新建商品
+    @UseGuards(AuthGuard('jwt'))
+    @UseInterceptors(new RbacInterceptor(role.DEVELOPER))
+    @Post('create')
+    async createCommodity(@Body() body: any, @Request() req: any) {
+        return await this.commodityService.createCommodity(body, req.user.username);
+    }
 
-  // 修改商品
-  @UseGuards(AuthGuard('jwt'))
-  @UseInterceptors(new RbacInterceptor(2))
-  @Post('update')
-  async updateCommodity(@Body() body: any, @Request() req: any) {
-    return await this.commodityService.updateCommodity(body, req.user.username);
-  }
+    // 修改商品
+    @UseGuards(AuthGuard('jwt'))
+    @UseInterceptors(new RbacInterceptor(role.DEVELOPER))
+    @Post('update')
+    async updateCommodity(@Body() body: any, @Request() req: any) {
+        return await this.commodityService.updateCommodity(body, req.user.username);
+    }
 
-  // 删除商品
-  @UseGuards(AuthGuard('jwt'))
-  @UseInterceptors(new RbacInterceptor(1))
-  @Post('delete')
-  async deleteCommodity(@Body() body: any) {
-    return await this.commodityService.deleteCommodity(body);
-  }
+    // 删除商品
+    @UseGuards(AuthGuard('jwt'))
+    @UseInterceptors(new RbacInterceptor(role.ADMIN))
+    @Post('delete')
+    async deleteCommodity(@Body() body: any) {
+        return await this.commodityService.deleteCommodity(body);
+    }
 }
